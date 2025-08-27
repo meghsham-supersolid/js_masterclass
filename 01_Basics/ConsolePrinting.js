b8aa8a2c4358517d1e1ef96ebee7438eb13a9b62
Meghsham <meghshamvinayakkapure@gmail.com>
2025-08-28 04:30:10 +0530

    started learning JS from Chai Aur Code

---
+++ 01_Basics/ConsolePrinting.js
@@ -0,0 +1,12 @@
+//print a single line
+
+let firstName = "Meghsham";
+let lastName = "Kapure";
+let age = 28;
+
+console.log(
+  "My name is " + firstName + " " + lastName + " and my age is " + age
+);
+
+//print ing tabular format
+console.table([firstName, lastName, age]);
---
+++ 01_Basics/VariableKeyword.js
@@ -0,0 +1,33 @@
+// using const
+const piConstant = 3.14;
+// they are used to define values that does not change once defined
+// redeclaration and reassigning of value is not allowed in constant
+
+// using var
+var someVarValue = 10;
+// var is variable that used used to store changing values
+// unlike const they can be redeclare and reassigned value
+
+// using let
+let someLetValue = 10;
+// let is also a variable that used used to store changing values
+// although its similar to var but it behaves differently as it can be reassigned value but can't redeclare the values with same variableName
+
+
+// with no keyword used
+someValue = 456;
+// when you don't give any keyword while declaring the variable the var properties and behavior is observed
+
+
+/* Note
+ - While declaring a constant value use const
+ - while declaring a variable use let instead of var as var have block and functional scoping issues, creating more issues in code
+- 
+
+
+
+
+ 
+ */
+
+  
